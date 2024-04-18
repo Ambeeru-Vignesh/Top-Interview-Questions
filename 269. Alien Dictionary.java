64662cb3ad917f13547e8fe4106d039d1aa81c0d
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-04-18 07:23:43 +0530

    initial commit

--- 269. Alien Dictionary.java
+++ 269. Alien Dictionary.java
@@ -1,8 +1,5 @@
 public class Solution {
-    /**
-     * @param words: a list of words
-     * @return: a string which is correct order
-     */
+    
     public String alienOrder(String[] words) {
         
         // Write your code here
