657bf1ac07701f036af676ed1eb0856c0462a2c1
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-04-27 04:47:58 +0530

    Optimal approach

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -1,3 +1,20 @@
+//Optimal Approach
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 1;
+        int c;
+
+        for (int i = 0; i < n - 1; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
+// Bottom-up Approach
 class Solution {
     public int climbStairs(int n) {
 
