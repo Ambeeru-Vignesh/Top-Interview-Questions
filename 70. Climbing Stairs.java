0ba11c27d3fa3ae2f00bd506e56fed88fd96a43a
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-04-27 04:52:59 +0530

    Top-down Approach

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -28,3 +28,32 @@ class Solution {
         return dp[n];
     }
 }
+
+// Top-down Approach
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return climbStairs(n - 1, dp) + climbStairs(n - 2, dp);
+    }
+
+    public int climbStairs(int n, int[] dp) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 0 || n == 1) {
+            dp[n] = 1;
+            return dp[n];
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = climbStairs(n - 1, dp) + climbStairs(n - 2, dp);
+        return dp[n];
+    }
+}
